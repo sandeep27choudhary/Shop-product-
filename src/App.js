ae3b5f55fbbdb051732543e36b3306eccf1c894b
sandeep kr choudhary <csandeep497@gmail.com>
2021-05-16 20:54:53 +0530

    react-application ui product details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import classes from './App.module.css';
+import ProductData from './ProductData'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <nav className={classes.Topbar}>
+         <img src="https://imgur.com/a/AZMwXZc.png"
+         alt="logo"/>
+       </nav>
       </header>
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
+        </div>
+
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+          <h3 className={classes.SectionHeading}>Select color</h3>
+          <div>
+          <img className={[classes.ProductImage, classes.SelectiveProductImage].join(' ')} 
+          src={"https://imgur.com/iOeUBV7.png"} alt="Black Color Watch"/>
+          <img className={classes.ProductImage} src={"https://imgur.com/PTgQlim.png"} alt="Red Color Watch"/>
+          <img className={classes.ProductImage} src={"https://imgur.com/Mplj1YR.png"} alt="Blue Color Watch"/>
+          <img className={classes.ProductImage} src={"https://imgur.com/xSIK4M8.png"} alt="Purple Color Watch"/>
+
+          </div>
+         
+        </div>
+      </div>
     </div>
   );
 }
