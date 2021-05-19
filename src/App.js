64dbb169c79e6c65b086264153962b85e80209d4
sandeep kr choudhary <csandeep497@gmail.com>
2021-05-19 16:11:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import classes from './App.module.css';
-import ProductData from './ProductData'
+import ProductData from './ProductData';
+import ProductDetails from './ProductDetails'
+import ProductPreview from './ProductPreview';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,27 +14,17 @@ function App() {
          alt="logo"/>
        </nav>
       </header>
+
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
-          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
+        <ProductPreview/>
         </div>
 
-        <div className={classes.ProductData}>
-          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-          
-          <p className={classes.ProductDescription}>{ProductData.description}</p>
+      <div>
+        <ProductDetails/>
+      </div>
 
-          <h3 className={classes.SectionHeading}>Select color</h3>
-          <div>
-          <img className={[classes.ProductImage, classes.SelectiveProductImage].join(' ')} 
-          src={"https://imgur.com/iOeUBV7.png"} alt="Black Color Watch"/>
-          <img className={classes.ProductImage} src={"https://imgur.com/PTgQlim.png"} alt="Red Color Watch"/>
-          <img className={classes.ProductImage} src={"https://imgur.com/Mplj1YR.png"} alt="Blue Color Watch"/>
-          <img className={classes.ProductImage} src={"https://imgur.com/xSIK4M8.png"} alt="Purple Color Watch"/>
 
-          </div>
-         
-        </div>
       </div>
     </div>
   );
---
+++ src/ProductDetails.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import classes from './ProductDetails.modlule.css';
+import ProductData from './ProductData';
+
+const ProductDetails = (props) => {
+return(
+    <div className={classes.ProductData}>
+    <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+    
+    <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+    <h3 className={classes.SectionHeading}>Select color</h3>
+    <div>
+    <img className={[classes.ProductImage, classes.SelectiveProductImage].join(' ')} 
+    src={"https://imgur.com/iOeUBV7.png"} alt="Black Color Watch"/>
+    <img className={classes.ProductImage} src={"https://imgur.com/PTgQlim.png"} alt="Red Color Watch"/>
+    <img className={classes.ProductImage} src={"https://imgur.com/Mplj1YR.png"} alt="Blue Color Watch"/>
+    <img className={classes.ProductImage} src={"https://imgur.com/xSIK4M8.png"} alt="Purple Color Watch"/>
+
+    </div>
+    <h3 className={classes.SectionHeading}>Features</h3>
+    <div>
+      <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
+      <button className={classes.FeatureItem}>Heart Rate</button>
+    </div>
+    
+    <button className={classes.PrimaryButton}>Buy Now</button>
+   
+  </div>
+)
+}
+export default ProductDetails;
