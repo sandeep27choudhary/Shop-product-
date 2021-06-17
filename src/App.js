971e460eac587fa9aff5c80043b7fbabaeb0cd56
sandeep kr choudhary <csandeep497@gmail.com>
2021-06-17 12:33:21 +0530

    added screenshot

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import classes from './App.module.css';
-import ProductDetails from './ProductDetails'
-import ProductPreview from './ProductPreview';
+import React from "react";
+import classes from "./App.module.css";
+import ProductDetails from "./ProductDetails";
+import ProductPreview from "./ProductPreview";
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-       <nav className={classes.Topbar}>
-         <img src="https://imgur.com/a/AZMwXZc.png"
-         alt="logo"/>
-       </nav>
+        <nav className={classes.Topbar}>
+          <img src="https://imgur.com/a/AZMwXZc.png" alt="logo" />
+        </nav>
       </header>
 
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
-        <ProductPreview/>
+          <ProductPreview />
         </div>
 
-      <div>
-        <ProductDetails/>
-      </div>
-      
+        <div>
+          <ProductDetails />
+        </div>
       </div>
     </div>
   );
--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -1,27 +1,31 @@
+import React from "react";
 
-import React from 'react';
+import classes from "./ProductPreview.module.css";
 
-import classes from './ProductPreview.module.css';
-
-const currentHour= new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
-const currentMinutes= new Date().getMinutes()>9 ? new Date().getMinutes() : '0'+new Date().getMinutes();
-
+const currentHour =
+  new Date().getHours() > 9
+    ? new Date().getHours()
+    : "0" + new Date().getHours();
+const currentMinutes =
+  new Date().getMinutes() > 9
+    ? new Date().getMinutes()
+    : "0" + new Date().getMinutes();
 
 const ProductPreview = (props) => {
-    return (
-        <div className={classes.ProductPreview}>
-        <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
+  return (
+    <div className={classes.ProductPreview}>
+      <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
 
-          {/*<div className={classes.TimeSection}>
+      {/*<div className={classes.TimeSection}>
             <p>{`${currentHour}:${currentMinutes}`}</p>
           </div>*/}
 
-          <div className={classes.HeartBeatSection}>
-          <i class="fas fa-heartbeat"></i>
-            <p>78</p>
-          </div>
-          </div>
-    )
-}
+      <div className={classes.HeartBeatSection}>
+        <i class="fas fa-heartbeat"></i>
+        <p>78</p>
+      </div>
+    </div>
+  );
+};
 
 export default ProductPreview;
